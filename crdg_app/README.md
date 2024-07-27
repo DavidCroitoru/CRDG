3adb3f924537a59aa9c2720ddf4bf92854255fcc
DavidCroitoru <croitorudavid@cni-sv.ro>
2024-07-27 21:25:11 +0300

    add flutter app

---
+++ crdg_app/README.md
@@ -0,0 +1,3 @@
+# crdg_app
+
+A new Flutter project.
---
+++ crdg_app/lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:crdg_app/crdg.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MainApp());
+}
+
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: crdg(),
+       
+    );
+  }
+}
